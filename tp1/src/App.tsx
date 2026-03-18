5c1500c14e2cad3dc0aa5adb2c089491cbad38b7
Fella Bennounas <bennounas@unistra.fr>
2026-03-18 11:57:20 +0100

    Tp3 fini sauf partie Option

--- tp1/src/App.tsx
+++ tp1/src/App.tsx
@@ -1,36 +1,84 @@
 import { useState } from "react";
 
-export function ItemsApp() {
-  const [items, setItems] = useState<string[]>([]);
+type GroceryItem = {
+  id: number;
+  name: string;
+  cost: number;
+  quantity: number;
+};
 
-  function addItem(newItem: string) {
+type Changes = {
+  name: string;
+  cost: number;
+  quantity: number;
+};
+
+type ChangesPreview = Omit<Changes, "name" | "cost" | "quantity">;
+
+export function GroceryApp() {
+  const [items, setItems] = useState<GroceryItem[]>([]);
+  const [nextId, setNextId] = useState(1);
+
+  function addItem(nom: string) {
+    const newItem: GroceryItem = {
+      id: nextId,
+      name: nom,
+      cost: 1,
+      quantity: 1,
+    };
     setItems([...items, newItem]); //new items at the end of the array
+    setNextId(nextId + 1);
   }
 
-  function removeItem(index: number) {
-    setItems(items.filter((item, i) => i != index));
+  function removeItem(id: number) {
+    setItems(items.filter((item) => item.id != id));
+  }
+
+  function updateItem(id: number, changes: Changes) {
+    setItems(
+      items.map((item) => {
+        if (item.id !== id) {
+          return item;
+        }
+        return { ...item, ...changes };
+      }),
+    );
   }
 
   return (
     <div>
-      <AddItemForm addItem={addItem} />
-      <ItemsList items={items} removeItem={removeItem} />
+      <div>
+        <AddItemForm addItem={addItem} />
+        <ItemsList
+          items={items}
+          removeItem={removeItem}
+          updateItem={updateItem}
+        />
+        <p>
+          Coût total :{" "}
+          {items.reduce((total, item) => total + item.cost * item.quantity, 0)}
+        </p>
+      </div>
     </div>
   );
 }
 
 type ItemsListProps = {
-  items: string[];
-  removeItem: (index: number) => void;
+  items: GroceryItem[];
+  removeItem: (id: number) => void;
+  updateItem: (id: number, changes: Changes) => void;
 };
 
-export function ItemsList({ items, removeItem }: ItemsListProps) {
+export function ItemsList({ items, removeItem, updateItem }: ItemsListProps) {
   return (
     <ul>
-      {items.map((item, index) => (
-        <li key={index}>
-          {item}
-          <button className="small danger" onClick={() => removeItem(index)}>
+      {items.map((item) => (
+        <li key={item.id}>
+          <UpdateItem
+            item={item}
+            updateItem={(changes) => updateItem(item.id, changes)}
+          />
+          <button className="small danger" onClick={() => removeItem(item.id)}>
             Supprimer
           </button>
         </li>
@@ -66,7 +114,44 @@ function AddItemForm({ addItem }: AddItemFormProps) {
 export default function App() {
   return (
     <div>
-      <ItemsApp />
+      <GroceryApp />
     </div>
+  );
+}
+
+type ItemProps = {
+  item: GroceryItem;
+  updateItem: (changes: Changes) => void;
+};
+
+function UpdateItem({ item, updateItem }: ItemProps) {
+  const [shouldUpdate, setShouldUpdate] = useState(false); //prop uniquement pour modif
+
+  const handleSubmit: SubmitEventHandler<HTMLFormElement> = function (event) {
+    event.preventDefault();
+    const form = event.currentTarget;
+    updateItem({
+      name: form.nom.value,
+      cost: Number(form.prix.value),
+      quantity: Number(form.qtt.value),
+    });
+    setShouldUpdate(false);
+  };
+
+  return (
+    <form onSubmit={handleSubmit} onChange={() => setShouldUpdate(true)}>
+      <label>name</label>
+      <input name="nom" type="text" defaultValue={item.name} />
+
+      <label>cost</label>
+      <input name="prix" type="number" defaultValue={item.cost} />
+
+      <label>quantity</label>
+      <input name="qtt" type="number" defaultValue={item.quantity} />
+
+      <button type="submit" disabled={!shouldUpdate}>
+        Modifier
+      </button>
+    </form>
   );
 }
